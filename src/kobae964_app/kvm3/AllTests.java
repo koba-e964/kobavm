e99ea72ab85a938f693f190fede8c484fd1f2d53
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-06-20 22:16:11 +0900

    (minor)class AllTests
    Added Test Suite(AllTests)

---
+++ src/kobae964_app/kvm3/AllTests.java
@@ -0,0 +1,11 @@
+package kobae964_app.kvm3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CallStackTest.class, ClassLoaderTest.class, CPUTest.class })
+public class AllTests {
+
+}
