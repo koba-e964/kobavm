6b7bdc3746e7e1eb7420fe8b9cb958223dd37bc5
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-06-27 11:29:20 +0900

    (Refactoring)CallStackTest
    Added some tests to CallStackTest.
    All tests in CallStackTest succeed.

--- src/kobae964_app/kvm3/CallStackTest.java
+++ src/kobae964_app/kvm3/CallStackTest.java
@@ -1,6 +1,9 @@
 package kobae964_app.kvm3;
 
 import static org.junit.Assert.*;
+
+import java.util.Random;
+
 import kobae964_app.kvm3.CallStack.DataTypeMismatchException;
 
 import org.junit.Test;
@@ -47,6 +50,32 @@ public class CallStackTest {
 		assertEquals(3.0, r, 1e-9);
 		String v=cs.popString();
 		assertEquals("xfyxfxg",v);
+	}
+	@Test
+	public void testPushPop1(){
+		CallStack cs=new CallStack();
+		cs.pushReal(2.2360679);
+		double r=cs.popReal();
+		assertEquals(2.2360679, r, 1e-9);
+	}
+	/**
+	 * Random test of pushInt/pushReal/popInt/popReal
+	 */
+	@Test
+	public void testPushPop2(){
+		final int n=10000;
+		Random rnd=new Random();
+		CallStack cs=new CallStack();
+		for(int i=0;i<n;i++){
+			long val=rnd.nextLong();
+			cs.pushInt(val);
+			assertEquals(val, cs.popInt());
+		}
+		for(int i=0;i<n;i++){
+			double value=rnd.nextDouble();
+			cs.pushReal(value);
+			assertEquals(value,cs.popReal(),1e-9);
+		}
 	}
 	
 }
