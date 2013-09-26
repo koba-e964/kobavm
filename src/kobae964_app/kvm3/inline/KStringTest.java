b3d04a594a0cf32d529c4671d65f1e7e7e016749
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-09-26 11:50:11 +0900

    KStringTest/callInstance
    Automation of tests for instance method.
    callInstance() serves as a kind of compiler.

--- src/kobae964_app/kvm3/inline/KStringTest.java
+++ src/kobae964_app/kvm3/inline/KStringTest.java
@@ -2,7 +2,11 @@ package kobae964_app.kvm3.inline;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import kobae964_app.kvm3.CPU;
+import kobae964_app.kvm3.CallStack;
 import kobae964_app.kvm3.DataType;
 import kobae964_app.kvm3.Mem;
 import kobae964_app.kvm3.VarEntry;
@@ -13,28 +17,59 @@ import org.junit.Test;
 
 public class KStringTest {
 
-	@SuppressWarnings("deprecation")
 	@Test
 	public void testCallLength() {
 		String method="length";
 		String str="Kaehler";
+		VarEntry result=callInstance(new KString(str), method);
+		assertEquals(new VarEntry(DataType.INT,str.length()),result);
+	}
+	@SuppressWarnings("deprecation")
+	VarEntry callInstance(KString instance,String method,Object... args){
 		VarEntry methodV=new VarEntry(DataType.OBJECT,new KString(method).getAddress());
-		VarEntry strV=new VarEntry(DataType.OBJECT,new KString(str).getAddress());
+		VarEntry strV=new VarEntry(DataType.OBJECT,instance.getAddress());
+		List<VarEntry> argList=new ArrayList<VarEntry>();
+		for(Object a:args){
+			VarEntry ve;
+			if(a instanceof Integer){
+				ve=new VarEntry(DataType.INT,(Integer)a);
+			}else if(a instanceof Boolean){
+				ve=new VarEntry(DataType.BOOL,((Boolean)a)?1:0);
+			}else if(a instanceof String){
+				ve=new VarEntry(DataType.OBJECT,new KString((String)a).getAddress());
+			}else{
+				throw new IllegalArgumentException("arg:"+a.toString());
+			}
+			argList.add(ve);
+		}
 		Mem mem=new Mem(0x10000);
 		CPU cpu=new CPU(mem);
-		byte[] code={
+		VariableTable vt=cpu.getVTable();
+		vt.store(0, methodV);
+		vt.store(1, strV);
+		byte[] preCode={
 			LDV,0,0,0,//[0]="length"
 			LDV,1,0,0,//[1]="Kaehler"
 			CALLin,0,0,0,//call
 			-1,0,0,0,//EXIT
 		};
+		byte[] code=new byte[4*argList.size()+preCode.length];
+		//link
+		for(int i=0,s=argList.size();i<s;i++){
+			vt.store(i+2,argList.get(i));
+			byte[] snip=new byte[]{LDV,(byte)(i+2),(byte)((i+2)>>>8),0};//[i+2]
+			System.arraycopy(snip, 0, code, 4*i, 4);
+		}
+		System.arraycopy(preCode, 0, code, 4*argList.size(), preCode.length);
 		mem.load(code,0);
-		VariableTable vt=cpu.getVTable();
-		vt.store(0, methodV);
-		vt.store(1, strV);
 		cpu.run();
-		assertEquals(1,cpu.getCallStack().size());
-		assertEquals(new VarEntry(DataType.INT,str.length()),cpu.getCallStack().getAt(0));
+		CallStack cs=cpu.getCallStack();
+		if(cs.size()==0){
+			return null;
+		}
+		//cs.size()>=1
+		assertEquals(1,cs.size());
+		return cs.getAt(0);
+		
 	}
-
 }
