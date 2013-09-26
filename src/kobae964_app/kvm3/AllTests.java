a471e3e6cd408e8e153c45838aeff110aeb18621
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-09-26 17:00:59 +0900

    add KStringTest to AllTests

--- src/kobae964_app/kvm3/AllTests.java
+++ src/kobae964_app/kvm3/AllTests.java
@@ -1,6 +1,7 @@
 package kobae964_app.kvm3;
 
 import kobae964_app.kvm3.inline.IOTest;
+import kobae964_app.kvm3.inline.KStringTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -8,7 +9,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CallStackTest.class, ClassLoaderTest.class, CPUTest.class,
-		VarEntryTest.class ,IOTest.class})
+		VarEntryTest.class,KStringTest.class ,IOTest.class})
 public class AllTests {
 
 }
