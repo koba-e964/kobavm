04538ace077dfa6c498f8c7d3dd834146f9498ce
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-10-01 16:34:14 +0900

    Add inline/KArrayTest to AllTests

--- src/kobae964_app/kvm3/AllTests.java
+++ src/kobae964_app/kvm3/AllTests.java
@@ -1,6 +1,7 @@
 package kobae964_app.kvm3;
 
 import kobae964_app.kvm3.inline.IOTest;
+import kobae964_app.kvm3.inline.KArrayTest;
 import kobae964_app.kvm3.inline.KStringTest;
 
 import org.junit.runner.RunWith;
@@ -9,7 +10,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CallStackTest.class, ClassLoaderTest.class, CPUTest.class,
-		VarEntryTest.class,KStringTest.class ,IOTest.class})
+		VarEntryTest.class,KArrayTest.class, KStringTest.class ,IOTest.class})
 public class AllTests {
 
 }
