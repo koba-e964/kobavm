a5c44fdca9eadf35371b2e74bb6416eaecc2c876
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-11-17 12:16:05 +0900

    testExe in LoaderTest

--- src/kobae964_app/kvm3/binclz/LoaderTest.java
+++ src/kobae964_app/kvm3/binclz/LoaderTest.java
@@ -1,5 +1,7 @@
 package kobae964_app.kvm3.binclz;
 
+import static kobae964_app.kvm3.CPU.LDCim;
+import static kobae964_app.kvm3.CPU.RET;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
@@ -78,6 +80,16 @@ public class LoaderTest {
 				new Object[]{(Long)0x9ae8563412L,"abelsch",(Double)0.0,
 				new long[]{0x0807060504030201L,0x570000130000009fL}});
 	}
+	byte[] intsToBytes(int[] array){
+		byte[] result=new byte[array.length*4];
+		for(int i=0;i<array.length;i++){
+			int v=array[i];
+			for(int j=0;j<4;j++){
+				result[4*i+j]=(byte)(v>>>(8*j));
+			}
+		}
+		return result;
+	}
 	@Test
 	public void testFields() {
 		byte[] source={'k','v','m',0x7f,//header
@@ -110,5 +122,58 @@ public class LoaderTest {
 				new Object[]{(Long)0x9ae8563412L,"test0","I",});
 		assertArrayEquals(new String[]{"test0"},sol.bdat.fieldNames);
 	}
-
+	@Test
+	public void testExe(){
+		byte[] header={'k','v','m',0x7f};
+		assert header.length==4;
+		int[] code=new int[]{
+			LDCim+256*1,//LDC.im 1
+			RET+0x100,//RET st[0]=>1
+		};
+		int headInfoLen=32;//bytes
+		int cpoolLen=24;
+		int codePos=header.length+headInfoLen;
+		int methodPos=codePos+4*code.length+cpoolLen;
+		int methodLen=16;
+		int namePos=methodPos+methodLen;
+		byte[] name="main".getBytes();
+		int signPos=namePos+name.length;
+		byte[] sign="".getBytes();
+		int[] cpool=new int[]{
+			Loader.STRING,namePos,name.length,
+			Loader.STRING,signPos,sign.length,
+		};
+		assert 4*cpool.length==cpoolLen;
+		int[] method=new int[]{
+			0,//name-sid:"main"
+			1,//num-var:1
+			0,//offset:relative to codePos
+			1,//sign-sid:""
+		};
+		assert 4*method.length==methodLen;
+		int[] field=new int[0];
+		int[] headInfo=new int[]{
+			codePos,//code-place
+			4*code.length,//code-length
+			codePos+4*code.length,//cpool-place
+			4*cpool.length,//cpool-length
+			methodPos,//method-place
+			4*method.length,
+			methodPos+4*method.length,//field-place
+			4*field.length,//field-length
+		};
+		assert 4*headInfo.length==headInfoLen;
+		byte[] ld=new byte[signPos+sign.length];
+		System.arraycopy(header, 0, ld, 0, header.length);
+		System.arraycopy(intsToBytes(headInfo), 0, ld, header.length, headInfoLen);
+		System.arraycopy(intsToBytes(code), 0, ld,codePos,4*code.length);
+		System.arraycopy(intsToBytes(cpool), 0, ld, codePos+4*code.length, cpoolLen);
+		System.arraycopy(intsToBytes(method), 0, ld, methodPos,methodLen);
+		System.arraycopy(name, 0, ld, namePos, name.length);
+		System.arraycopy(sign, 0, ld, signPos, sign.length);
+		Loader sol=new Loader(ld);
+		assertArrayEquals(new String[]{"main",""},sol.bdat.constPool);
+		assertArrayEquals(new String[]{"main"},sol.bdat.methodNames);
+		assertArrayEquals(new int[]{0},sol.bdat.methodOffsets);
+	}
 }
